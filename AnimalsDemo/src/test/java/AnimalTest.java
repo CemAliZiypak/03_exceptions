58ecf4d6bef7fd1663c11608ec1333ebbf313f76
Cem Ali Ziypak <91340699+CemAliZiypak@users.noreply.github.com>
2021-11-07 13:02:29 +0100

    Add files via upload

---
+++ AnimalsDemo/src/test/java/AnimalTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnimalTest {
+
+    int a;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        a = 1;
+    }
+    @Test
+    void myFirstTest(){
+        assertTrue(a > 0);
+
+    }
+
+}
